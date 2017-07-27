d3a95d60d30f40f81eafedbbae61e66325e8a6c0
cedricode <cedric.yang@ihealthlabs.com>
2017-07-27 15:08:20 +0200

    8e

--- 8E.cpp
+++ 8E.cpp
@@ -1,15 +1,19 @@
+// "This answer exceeds memory limit... should be prevented earlier"
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <set>
+#include <map>
+#include <utility>
 
 using namespace std;
 
 
-int n, k;
+long long n, k;
 vector<int> state(0);
 vector<int> memory(0);
-int count = 0, target = 0;
-
+long long count = 0, target = 0;
+long long base;
 
 bool c_b_smaller() {
 	long long k = 0, k1 = 0, k2 = 0, k3 = 0;
@@ -58,69 +62,120 @@ void dfs(int l) {
 	state[l] = 0;
 }
 
+void add_holes(set<int> &holes, unsigned num) {
+	int reverse_num = 0, i, temp;
+	int num_of_bits = n / 2;
+	for (i = 0; i < num_of_bits; i++) {
+		temp = (num& (1<<i));
+		if (temp) reverse_num |= (1 << ((num_of_bits - 1) - i));
+	}
+	holes.insert(reverse_num);
+	holes.insert(base - 1 - reverse_num);
+}
+
+void add_holes_odd(set<int> &holes, unsigned num) {
+	int reverse_num = 0, i, temp;
+	int num_of_bits = n / 2;
+	unsigned num2 = num;
+	num >>= 1;
+	for (i = 0; i < num_of_bits; i++) {
+		temp = (num& (1<<i));
+		if (temp) reverse_num |= (1 << ((num_of_bits - 1) - i));
+	}
+	if (num2 & 1) {
+		holes.insert(reverse_num);	
+	} else {
+		holes.insert(base - 1 - reverse_num);
+	}
+}
+
 
 int main(void) {
-	cin >> n >> k;
+	// cin >> n >> k;
+	n = 49, k = 40394027154620;
+
 	state = vector<int>(n, 0);
+	base = pow(2, n/2);	
 
-	// int factor = 1;
-	// if (n & 1) { // odd
-	// 	factor = 1;
-	// } else {
-	// 	factor = 2;
-	// }
+	int factor = 1;
+	if (n & 1) { // odd
+		factor = 1;
+	} else {
+		factor = 2;
+	}
 
-	// long long base = pow(2, n/2);
 
-	// if (k > (base + base * (base / factor -1) / 2 - 1)) {
-	// 	cout << -1 << endl;
-	// 	return 0;
-	// }
+	if (k > (base + base * (base / factor -1) / 2 - 1)) {
+		cout << -1 << endl;
+		return 0;
+	}
 
-	// long long start = base - 1;
-	// int i = 0;
-	// for (; i < base / factor; i++) {
-	// 	if (k <= start) break;
-	// 	else start += (base - factor*(1+i));
-	// }
+	long long start = base - 1;
+	int i = 0;
+	for (; i < base / factor; i++) {
+		if (k <= start) break;
+		else start += (base - factor*(1+i));
+	}
 
-	// if (k <= base - 1) {
-	// 	int i = n - 1;
-	// 	while (k != 0) {
-	// 		state[i--] = k % 2;
-	// 		k /= 2;
-	// 	}
-	// } else {
-	// 	start -= (base - factor*i);
-	// 	count = start;
-	// 	int j = (n-1) / 2;
-	// 	while (i != 0) {
-	// 		state[j--] = i % 2;
-	// 		i /= 2;
-	// 	}
+	if (k <= base - 1) {
+		int i = n - 1;
+		while (k != 0) {
+			state[i--] = k % 2;
+			k /= 2;
+		}
+	} else {
+		start -= (base - factor*i);
+		count = start;
+		int j = (n-1) / 2;
+		int num_i = i;
+		while (i != 0) {
+			state[j--] = i % 2;
+			i /= 2;
+		}
 
-	// 	int numi = 0;
-	// 	while (count != k) {
-	// 		i = numi++;
-	// 		int j;
-	// 		for (j = (n-1) / 2 + 1; j < n; j++) state[j] = 0;
-	// 		j = n - 1;
-	// 		while (i != 0) {
-	// 			state[j--] = i % 2;
-	// 			i /= 2;
-	// 		}
-	// 		if (!c_b_smaller()) count++;
-	// 	}
+		set<int> holes;
+		map<int, int> holes_w_i;
 
-	// 	// dfs(n/2);
-		
-	// }
+		if (n&1) {
+			count = k - count - 1;
+			for (unsigned int j = 0; j < num_i; j++) {
+				add_holes_odd(holes, j);
+			}
+		} else {
+			count = k - count;	
+			for (unsigned int j = 1; j < num_i; j++) {
+				add_holes(holes, j);
+			}
+		}
 
-	// for (int i = 0; i < n; i++) cout << state[i];
-	// cout << endl;
+		j = 1;
+		holes_w_i.insert(make_pair(-1, 0));
+		set<int>::iterator itr = holes.begin();
+		while(itr != holes.end()) {
+			holes_w_i.insert(make_pair(*itr, j++));
+			holes.erase(itr++);
+		}
+
+		int previous = -1;
+		map<int, int>::iterator itrl = --holes_w_i.upper_bound(count);
+		while (itrl != holes_w_i.end() && itrl->first != previous) {
+			count += itrl->second - holes_w_i[previous];
+			previous = itrl->first;
+			itrl = --holes_w_i.upper_bound(count);
+		}
+
+		j = n - 1;
+		while (count != 0) {
+			if (count&1) state[j] = 1;
+			j--;
+			count >>= 1;
+		}
+	}
+
+	for (int i = 0; i < n; i++) cout << state[i];
+	cout << endl;
 	
-	// cout << (base + base* (base / factor - 1) / 2 - 1) << endl;
 
-	dfs(0);
-	cout << count << endl;
+	// dfs(0);
+	// cout << count << endl;
 }

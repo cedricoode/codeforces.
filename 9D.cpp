d0f3a4a04df402b67688009a89cf4dd7c1099f6a
cedricode <cedric.yang@ihealthlabs.com>
2017-07-31 18:31:01 +0200

    9d

---
+++ 9D.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <set>
+#include <utility>
+#include <iterator>
+
+using namespace std;
+
+typedef long long LL;
+typedef set<int> Nums;
+
+LL dp[36][36];
+LL total[36];
+int n, h;
+
+LL factorial(LL dep) {
+	if (dep == 0 || dep == 1) return 1;
+	return dep * factorial(dep-1);
+}
+
+LL compute_dp(int len, int dep, Nums &nums) {
+	// cout << "dp: " << len << " " << dep << endl;
+	if (len == 0 ) return 0;
+	else if (dep == 0 || dep == 1) {
+		return total[len];
+	}
+
+	LL sum = 0;
+	LL previous = 0;
+	for (Nums::iterator itr = nums.begin(); itr != nums.end(); itr++) {
+		int k1 = distance(nums.begin(), itr);
+		int k2 = distance(itr, nums.end()) - 1;
+		previous = sum;
+		if (dep-1 <= k1) {
+			if (dp[k1][dep-1]) {
+				sum += dp[k1][dep-1] * total[k2];
+			}else {
+				Nums n1(nums.begin(), itr);
+				sum += compute_dp(k1, dep-1, n1) * total[k2];
+			}
+		} 
+		
+		if (dp[k2][dep-1] || k2 < dep-1) 
+			sum += (total[k1] - dp[k1][dep-1]) * dp[k2][dep-1];
+		else {
+			itr++;
+			Nums n2(itr, nums.end());
+			itr--;
+			sum += (total[k1] - dp[k1][dep-1]) * compute_dp(k2, dep-1, n2);
+		}
+		// if (dep == h)
+			// cout << len << " " << dep << " " << *itr << " " << sum - previous << endl;
+	}
+	return dp[len][dep] = sum;
+}
+
+
+int main(void) {
+	cin >> n >> h;
+	Nums nums;
+	total[0] = total[1] = 1;
+	for (int i = 2; i <= n; i++) {
+		LL sum = 0;
+		for (int j = 1; j <= i; j++) {
+			sum += total[j-1] * total[i-j];
+		}
+		total[i] = sum;
+		cout << total[i] << endl;
+	}
+	for (int i = 1; i <= n; i++) nums.insert(i);
+	cout << compute_dp(n, h, nums) << endl;
+}

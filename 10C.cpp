3dc238af1e41732de0323fcd6dbc86e34abf4b06
cedricode <cedric.yang@ihealthlabs.com>
2017-08-03 16:42:29 +0200

    10c

--- 10C.cpp
+++ 10C.cpp
@@ -1,20 +1,49 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int n;
 
+int count(int t) {
+	return n >= t ? (n - t) / 9 + 1 : 0;
+}
+
+typedef long long LL;
+
 int main(void) {
 	cin >> n;
-	double count = 0;
-	for (double i = 1; i < n+1; i++) {
-		for (double j = i; j < n+1; j++) {
-			double mul = i * j;
-			int ri = i % 9;
-			int rj = i % 9;
+	LL sum = 0;
+	LL bound = sqrt(double(n));
+	for (int i = 1; i <= bound; ++i) {
+		int ri = (i % 9);
+		int rc = (ri * ri) % 9;
+		rc = rc ? rc : 9;
+		sum += count(rc) - 1;
+		for (int j = i+1; j <= bound; ++j) {
+			int ri = i % 9, rj = j % 9;
 			int rc = (ri * rj) % 9;
-			int rm = mul % 9;
-			
+			rc = rc ? rc : 9;
+			int c = count(rc);
+			sum += 2 * count(rc) - 2;
 		}
 	}
+
+
+	for (int i = bound; i <= n; ++i) {
+		int ri = (i % 9);
+		int rc = (ri * ri) % 9;
+		rc = rc ? rc : 9;
+		if (i != bound)
+			sum += count(rc);
+		for (int j = i + 1; j <= n; ++j) {
+			int ri = i % 9, rj = j % 9;
+			int rc = (ri * rj) % 9;
+			rc = rc ? rc : 9;
+			int c = count(rc);
+			if (c) cout << i << " " << j << ": " << c << endl;
+			sum += 2 * count(rc);
+		}
+	}
+	cout << sum << endl;
 }
